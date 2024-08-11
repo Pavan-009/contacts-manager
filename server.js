95c65f0a3e0bb7c7a8d405911bd2ecfef2208239
Pavan_Kumar <pavankumar.99gpk@gmail.com>
2024-08-11 08:52:12 +0530

    Changes in server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const errorHandler = require("./middleWare/errorHandler");
 const dotenv = require('dotenv').config()
 const port = process.env.PORT ||5000;
 const app = express();
-app.use(express.json());
+app.use(express.json());    
 app.use('/api/contacts/',require('./routes/contactRoute'));
 app.use(errorHandler);
 app.listen(port,()=>{
